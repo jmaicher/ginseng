73f316eea0dea2ca792f3d6013e010320272fcd1
Julian Maicher <mail@jmaicher.de>
2012-06-11 11:06:44 +0200

    Added integration tests for core extensions

---
+++ specs/integration/extensions_spec.js
@@ -0,0 +1,57 @@
+require(['ginseng'], function(Ginseng) {
+  'use strict';
+
+  describe('core extensions', function() {
+
+    var Core, Extension, Module,
+        core, module;
+ 
+    Extension = Ginseng.Extension.extend({
+    
+      initialize: function() {
+        this.core.foo = this.foo;
+      },
+
+      foo: function() {
+        return 'bar';
+      },
+
+      Sandbox: {
+        foo: function() {
+          return this.core.foo();
+        }
+      }
+    
+    });
+
+    Core = Ginseng.Core.extend({
+    
+      initialize: function() {
+        this.addExtension(Extension);
+      }
+
+    });
+
+    Module = Ginseng.Module.extend({
+    
+      initialize: function() {
+        var foo = this.sandbox.foo();
+      }
+
+    });
+
+    beforeEach(function() {
+      core = new Core();
+    });
+
+    it('expose Sandbox functions which are available to modules', function() {
+      var spy = spyOn(core, 'foo');
+      
+      core.addModule(Module);
+
+      expect(spy).toHaveBeenCalled();
+    });
+  
+  });
+
+});
